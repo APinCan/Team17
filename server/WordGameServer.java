0b9a13bd88d3b9016ad14d3b9eebc0237f128c1f
ApinCan <strain2011@gmail.com>
2020-05-24 17:15:29 +0900

    add simple server

--- server/WordGameServer.java
+++ server/WordGameServer.java
@@ -1,10 +1,30 @@
 package server;
 
-public class WordGameServer {
+import kr.ac.konkuk.ccslab.cm.stub.CMServerStub;
 
+public class WordGameServer {
+	private CMServerStub m_serverStub;
+	private WordGameEventHandler m_eventHandler;
+	
+	public WordGameServer() {
+		m_serverStub = new CMServerStub();
+		m_eventHandler = new WordGameEventHandler(m_serverStub);
+	}
+	
+	public CMServerStub getServerStub() {
+		return m_serverStub;
+	}
+	
+	public WordGameEventHandler getServerEvenetHandler() {
+		return m_eventHandler;
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		WordGameServer server = new WordGameServer();
+		CMServerStub cmStub = server.getServerStub();
+		cmStub.setAppEventHandler(server.getServerEvenetHandler());
+		cmStub.startCM();
 	}
-
+	
 }
